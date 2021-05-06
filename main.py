8ed29c2019d8ee567138ee8c018cfc201c66e894
unknown <johnparks552@gmail.com>
2021-05-06 04:42:04 -0500

    edited the main.py file, started the class Player, added clock, attempted background change

--- main.py
+++ main.py
@@ -16,22 +16,30 @@ pygame.display.set_caption("TechTime Racers") # Window name
 
 # Class for Techie, playable character
 class Player(pygame.sprite.Sprite):
-    run = [pygame.image.load(os.path.join("Assets/"))]
-    def __init__(self, vector):
+    run = [pygame.image.load(os.path.join("Assets/Techie", "Techie Run Frame 1.png")), 
+            pygame.image.load(os.path.join("Assets/Techie", "Techie Run Frame 2.png"))]
+    jump = pygame.image.load(os.path.join("Assets/Techie", "Techie Jump.png"))
+    duck = pygame.image.load(os.path.join("Assets/Techie", "Techie Duck.png"))
+    def __init__(self, width, height, x_pos, y_pos):
         pygame.sprite.Sprite.__init__(self)
-        
+        self.rect = self.image.get_rect()
+
+# Display dictionary
 
 # Background
 # NOTE: The background will eventually be changed to a dictionary to compensate for different levels
-bg = pygame.Surface(WINDOW.get_size())
-bg = bg.convert()
-bg.fill((250, 250, 250))
+bg = pygame.image.load(os.path.join("Assets/Backgrounds", "Inside_IESB.jpg")).convert()
 
+# Clock ticking for FPS (60fps)
+clock = pygame.time.Clock()
 
 # Main Event Loop
 run = True
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
+    pygame.display.flip()
 
+""" I started to work on some of the code for techie and what not, but I think im going to try and create a main menu first -John"""
