4f692d8775b05fd0c2cfecd719657d7602ddd336
unknown <jwp032@latech.edu>
2021-05-06 03:45:38 -0500

    slightly edited code and reorganized/renamed files git push git status this is funny because I just realized I forgot to end string

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from pygame.locals import *     # Further explanation can be found at: https://w
 pygame.init()
 
 # Constants
-WIDTH = 1280
-HEIGHT = 720
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
 
 # Creating the window of the game
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("TechTime Racers") # Window name
+
+
+# Class for Techie, playable character
+class Player(pygame.sprite.Sprite):
+    run = [pygame.image.load(os.path.join("Assets/"))]
+    def __init__(self, vector):
+        pygame.sprite.Sprite.__init__(self)
+        
 
 # Background
 # NOTE: The background will eventually be changed to a dictionary to compensate for different levels
 bg = pygame.Surface(WINDOW.get_size())
 bg = bg.convert()
 bg.fill((250, 250, 250))
+
 
 # Main Event Loop
 run = True
